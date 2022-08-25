cdaeda96b561275b2bab6fb14331db88216182d0
Cimorexave <sadeq.s502@gmail.com>
2022-08-25 19:36:20 +0430

    setup menu structure

--- src/routes/Menu.tsx
+++ src/routes/Menu.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Box, Fab } from '@mui/material';
+import React from 'react';
+import MenuButton from '../components/MenuButton';
+import "../styles/menu.css";
+import menuIconSvg from "../assets/menu-icon.svg";
+
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <Box id="viewport-menu" >
+      {/* Header */}
+      <Box sx={{ display: "flex", width: "100%",
+        height: "20%", position: "absolute", top: 0, alignItems: "center",
+        justifyContent: "space-between", backgroundColor: "transparent" }}>
+          <img id="logo" src={require("../assets/white-logo.png")} alt="logo" style={{ marginLeft: "4em" }} />
+          {/* menu button  */}
+          <img src={menuIconSvg} alt="menu-icon" id='menu-icon-svg' />
+        </Box>
+          {/* Main menu */}
+        <Box id="main">
+          {/* menu  */}
+          <Box id="menu">
+
+          </Box>
+          {/* image  */}
+          <Box>
+
+          </Box>
+        </Box>
+          {/* Footer */}
+          <Box>
+            Sharing is Caring
+          </Box>
+
+    </Box>
   )
 }
 

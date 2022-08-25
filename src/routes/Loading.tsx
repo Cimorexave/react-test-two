929a84a2edb29c28df6adce978d27e51a34518f2
Cimorexave <sadeq.s502@gmail.com>
2022-08-25 11:30:02 +0430

    loading welcome page

--- src/routes/Loading.tsx
+++ src/routes/Loading.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Welcome from '../components/Welcome';
 import "../styles/loading.module.css";
 
 const Loading = () => {
+    //states
+    const [welcoming, setWelcoming] = useState<boolean>(true)
+    //lifecycle management
+    useEffect(() => {
+        setTimeout(() => {setWelcoming(false)}, 8000)
+    }, [])
+
   return (
     <div>
+        {welcoming ? <Welcome /> : "" }
         <video src=".../public/fotage razak.mp4" controls >
             <p>Your browser doesn't support this video format...</p>
         </video>

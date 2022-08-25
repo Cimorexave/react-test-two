f23c080ce99a32dad59dcc06036d931a92521fe5
Cimorexave <sadeq.s502@gmail.com>
2022-08-25 14:55:13 +0430

    updated header styling

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,9 +10,9 @@ const Home = () => {
     <div className='home'>
       <Box sx={{ display: "flex", width: "100%",
       height: "25%", position: "fixed", top: 0, alignItems: "center",
-       justifyContent: "space-between" }}>
-        <img src="" alt="logo" style={{ marginLeft: "5em" }} />
-        <Box sx={{ display: "flex", alignItems: "center", gap: 3, marginRight: "5em" }}>
+       justifyContent: "space-between", backgroundColor: "transparent" }}>
+        <img id="logo" src={require("../assets/white-logo.png")} alt="logo" style={{ marginLeft: "4em" }} />
+        <Box sx={{ display: "flex", alignItems: "center", gap: 5, marginRight: "4em" }}>
           <h3>English</h3>
           <MenuButton />
         </Box>

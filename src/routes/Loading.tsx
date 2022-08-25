5c2d254fd37983927aa7b7e2d8a24d11cfb0aea0
Cimorexave <sadeq.s502@gmail.com>
2022-08-25 11:12:42 +0430

    updating styles

--- src/routes/Loading.tsx
+++ src/routes/Loading.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "../styles/loading.module.css";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div>
+        <video src=".../public/fotage razak.mp4" controls >
+            <p>Your browser doesn't support this video format...</p>
+        </video>
+    </div>
   )
 }
 

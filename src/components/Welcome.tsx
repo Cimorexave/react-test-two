c5bd9e7f49d1d756083e725f6110b9ed57b15004
Cimorexave <sadeq.s502@gmail.com>
2022-08-25 12:02:34 +0430

    modified styling

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "../styles/welcome.css";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className='welcome'>
+        frghjukjwlfsligslkfn
+    </div>
   )
 }
 
--- src/routes/Loading.tsx
+++ src/routes/Loading.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Welcome from '../components/Welcome';
+// import {Welcome} from '../components/index';
 import "../styles/loading.module.css";
 
 const Loading = () => {
@@ -7,11 +8,11 @@ const Loading = () => {
     const [welcoming, setWelcoming] = useState<boolean>(true)
     //lifecycle management
     useEffect(() => {
-        setTimeout(() => {setWelcoming(false)}, 8000)
+        setTimeout(() => {setWelcoming(false)}, 10000)
     }, [])
 
   return (
-    <div>
+    <div className='loading'>
         {welcoming ? <Welcome /> : "" }
         <video src=".../public/fotage razak.mp4" controls >
             <p>Your browser doesn't support this video format...</p>
